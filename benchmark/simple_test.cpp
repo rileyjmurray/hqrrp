276f27e7ef57ffc282579eb9156585aeb149be98
rileyjmurray <rjmurray@berkeley.edu>
2022-03-15 09:54:25 -0700

    simple mkl benchmark

--- benchmark/simple_test.cpp
+++ benchmark/simple_test.cpp
@@ -1,10 +1,14 @@
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <blas.hh>
 #include <hqrrp.h>
 #include <mkl.h>
 
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
 
 #define max( a, b ) ( (a) > (b) ? (a) : (b) )
 #define min( a, b ) ( (a) < (b) ? (a) : (b) )
@@ -12,6 +16,14 @@
 // #define int64_t lapack_int
 // ^ Might be needed if LAPACK++ is linked against a library in the LP64 model (as opposed to ILP64)
 
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
+auto t1 = high_resolution_clock::now();
+auto t2 = high_resolution_clock::now();
+auto t3 = duration_cast<milliseconds>(t2 - t1);
 
 // ============================================================================
 // Declaration of local prototypes.
@@ -24,54 +36,51 @@ static void set_pvt_to_zero( int64_t n_p, int64_t * buff_p );
 int main( int argc, char *argv[] ) {
   using namespace HQRRP;
   
-  int64_t     nb_alg, pp, m_A, n_A, mn_A, ldim_A, ldim_Q, info, lwork;
-  double  * buff_A, * buff_tau, * buff_Q, * buff_wk_qp4, * buff_wk_orgqr;
+  int64_t     nb_alg, pp, m_A, n_A, ldim_A, info, lwork;
+  double  * buff_A, * buff_tau, * buff_wk_qp4;
   int64_t     * buff_p;
 
   // Create matrix A, vector p, vector s, and matrix Q.
-  m_A      = 300;
-  n_A      = 100;
-  mn_A     = min( m_A, n_A );
+  m_A      = 5000;
+  n_A      = 5000;
   buff_A   = ( double * ) malloc( m_A * n_A * sizeof( double ) );
   ldim_A   = max( 1, m_A );
-
   buff_p   = ( int64_t * ) malloc( n_A * sizeof( int64_t ) );
-
   buff_tau = ( double * ) malloc( n_A * sizeof( double ) );
 
-  buff_Q   = ( double * ) malloc( m_A * mn_A * sizeof( double ) );
-  ldim_Q   = max( 1, m_A );
 
-  // Generate matrix.
+  // Populate the test matrix and run the randomized algorithm.
   genmat(m_A, n_A, buff_A, (uint64_t) 0);
-
-  // Initialize vector with pivots.
   set_pvt_to_zero( n_A, buff_p );
-  lwork       = max( 1, 128 * n_A );
-  buff_wk_qp4 = ( double * ) malloc( lwork * sizeof( double ) );
-
-  // Factorize matrix.
   printf( "%% Just before computing factorization.\n" );
-  // New factorization.
+  t1 = high_resolution_clock::now();
+  lwork = -1;
   dgeqp4( & m_A, & n_A, buff_A, & ldim_A, buff_p, buff_tau, 
           buff_wk_qp4, & lwork, & info );
-  // Current factorization.
-  // dgeqp3_( & m_A, & n_A, buff_A, & ldim_A, buff_p, buff_tau, 
-  //          buff_wk_qp4, & lwork, & info );
+  lwork = (int64_t) *buff_wk_qp4;
+  buff_wk_qp4 = ( double * ) malloc( lwork * sizeof( double ) );
+  dgeqp4( & m_A, & n_A, buff_A, & ldim_A, buff_p, buff_tau, 
+          buff_wk_qp4, & lwork, & info );
+  t2 = high_resolution_clock::now();
   printf( "%% Just after computing factorization.\n" );
-
   printf( "%% Info after factorization:      %d \n", info );
   printf( "%% Work[ 0 ] after factorization: %d \n", ( int64_t ) buff_wk_qp4[ 0 ] );
-
-  // Remove workspace.
+  std::cout << duration_cast<milliseconds>(t2 - t1).count() << "ms for HQRRP\n";
   free( buff_wk_qp4 );
+
+  // **RE**populate the test matrix and call MKL
+  genmat(m_A, n_A, buff_A, (uint64_t) 0);
+  set_pvt_to_zero(n_A, buff_p);
+  blas::scal(n_A, 0.0, buff_tau, 1);
+  t1 = high_resolution_clock::now();
+  lapack::geqp3(m_A, n_A, buff_A, ldim_A, buff_p, buff_tau);
+  t2 = high_resolution_clock::now();
+  std::cout << duration_cast<milliseconds>(t2 - t1).count() << "ms for MKL\n";
 
   // Free matrices and vectors.
   free( buff_A );
   free( buff_p );
   free( buff_tau );
-  free( buff_Q );
-
   printf( "%% End of Program\n" );
 
   return 0;
