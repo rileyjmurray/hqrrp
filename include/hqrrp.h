6147f75a4e58881bc503befc59fd957457dcce54
rileyjmurray <rjmurray@berkeley.edu>
2022-03-30 19:00:36 -0700

    follow-up on last commit

--- include/hqrrp.h
+++ include/hqrrp.h
@@ -1,6 +1,5 @@
 #ifndef LAPACK_HH
 #include <lapack.hh>
-#include <lapack/config.h>
 #define LAPACK_HH
 #endif
 
@@ -10,14 +9,14 @@
 
 namespace HQRRP {
 
-void dgeqpr(lapack_int m, lapack_int n, double *A, lapack_int lda, lapack_int *jpvt, double *tau);
+void dgeqpr(int64_t m, int64_t n, double *A, int64_t lda, int64_t *jpvt, double *tau);
 
-void dgeqpr( lapack_int * m, lapack_int * n, double * A, lapack_int * lda, lapack_int * jpvt, double * tau,
-         double * work, lapack_int * lwork, lapack_int * info );
+void dgeqpr( int64_t * m, int64_t * n, double * A, int64_t * lda, int64_t * jpvt, double * tau,
+         double * work, int64_t * lwork, int64_t * info );
 
-lapack_int hqrrp( lapack_int m_A, lapack_int n_A, double * buff_A, lapack_int ldim_A,
-        lapack_int * buff_jpvt, double * buff_tau,
-        lapack_int nb_alg, lapack_int pp, lapack_int panel_pivoting );
+int64_t hqrrp( int64_t m_A, int64_t n_A, double * buff_A, int64_t ldim_A,
+        int64_t * buff_jpvt, double * buff_tau,
+        int64_t nb_alg, int64_t pp, int64_t panel_pivoting );
 
 } // end namespace HQRRP
 
